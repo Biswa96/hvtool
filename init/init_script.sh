b5cd3a716f929b567ab259209ab2f11f83638e79
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-18 04:59:18 +0000

    init: Mount more directories

--- init/init_script.sh
+++ init/init_script.sh
@@ -2,6 +2,12 @@
 
 mount -t devtmpfs dev /dev
 mount -t proc proc /proc
+mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
 mount -t sysfs sysfs /sys
+mount -t cgroup2 cgroup2 /sys/fs/cgroup
+mount -t debugfs none /sys/kernel/debug
+
+mkdir -p /dev/pts
+mount -t devpts devpts /dev/pts
 
 exec /sbin/getty -n -l /bin/sh 115200 /dev/hvc1
